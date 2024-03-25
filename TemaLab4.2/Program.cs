42fe0d31de90c3642ec969672a81c205b919fe1c
AlphaBugger <zepciuceduard@gmail.com>
2024-03-25 02:35:23 +0200

    Add files via upload

---
+++ TemaLab4.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileName = "input.txt";
+
+        if (!File.Exists(fileName))
+        {
+            File.WriteAllText(fileName, "apple banana carrot avocado broccoli cabbage blueberry " +
+                "apricot pear melon grape plum orange banana watermelon kiwi pineapple " +
+                "raspberry blackberry strawberry fig lemon lime peach mango coconut");
+        }
+
+        string[] words = File.ReadAllText(fileName).Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string[][] wordArrays = new string[26][];
+
+        for (int i = 0; i < 26; i++)
+        {
+            wordArrays[i] = new string[0];
+        }
+
+        foreach (string word in words)
+        {
+            if (!string.IsNullOrWhiteSpace(word) && char.IsLetter(word[0]))
+            {
+                int index = char.ToLower(word[0]) - 'a';
+                wordArrays[index] = AddToArray(wordArrays[index], word);
+            }
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (wordArrays[i].Length > 0)
+            {
+                Console.WriteLine($"Componenta {Convert.ToChar('A' + i)}:");
+                Console.WriteLine(string.Join(", ", wordArrays[i]));
+                Console.WriteLine();
+            }
+        }
+        Console.ReadKey();
+    }
+
+    static string[] AddToArray(string[] array, string element)
+    {
+        Array.Resize(ref array, array.Length + 1);
+        array[array.Length - 1] = element;
+        return array;
+    }
+}
---
+++ TemaLab4.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TemaLab4.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TemaLab4.2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cbf6170-f0d0-4a86-baf6-e1d7710b612d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
